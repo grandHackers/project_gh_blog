bad6293a9f74e43ecc51a8369a79cd475d353564
Erika Yoon <erikaY@Erikas-Air.attlocal.net>
2016-04-07 15:05:25 -0700

    Mac os x fs seems case-insensitive or something - so last file name change from 'view' to 'View' wasn't committed. so going to name 'View' to 'Blarg' and then in the next commit change it back to 'View'

---
+++ src/components/mainPage/Blarg.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import NavBar from '../common/Navbar';
+import Footer from '../common/Footer';
+import Feed from '../../containers/Feed';
+import AddPostForm from '../../containers/AddPostForm';
+
+export default class View extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() { 
+        return (
+            <div>
+                <NavBar signedIn={false}/>
+                <AddPostForm />
+                <Feed />
+                <Footer />
+            </div>
+        );
+    }
+}
